dc6bac9c5f99a799cd5b9b144a6d256dfe5db26a
Aulia Permana <permanaaulia@gmail.com>
2024-10-11 16:23:33 +0700

    Added MongoDB connection and setup

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const Customer = require('./models/Customer'); // Mengimpor model
+const dotenv = require('dotenv');
+const Customer = require('./models/Customer');
+
+dotenv.config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 
-// Koneksi ke MongoDB
-mongoose.connect('mongodb://localhost:27017/customers', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
@@ -19,10 +21,9 @@ mongoose.connect('mongodb://localhost:27017/customers', {
   console.error('Error connecting to MongoDB', err);
 });
 
-// Route untuk mendapatkan semua data dari MongoDB
 app.get('/api/customers', async (req, res) => {
   try {
-    const customers = await Customer.find(); // Mengambil semua data dari koleksi customer_data
+    const customers = await Customer.find();
     res.json(customers);
   } catch (err) {
     res.status(500).json({ error: err.message });
